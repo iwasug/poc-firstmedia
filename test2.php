411a3b173464d275cf8cfbb2d3055745b958f5be
Iwa Sugriwa <iwa@radyalabs.com>
2020-03-22 09:14:49 +0700

    test2

---
+++ test2.php
@@ -0,0 +1,39 @@
+<?php
+
+date_default_timezone_set('Asia/Jakarta');
+$date = date('Y-m-d H:i:s');
+$formula = $date . '#FM#Freshwork#2020@fc9631fFreshwork';
+$token = md5($formula);
+//echo $formula . '</br>' . $token;
+if (!empty($_POST['account'])) {
+    $url = "https://fm1.firstmedia.com/FMCOMAPIRest/Content/Homepage";
+
+    $account = $_POST['account'];
+
+    $jsonData = array(
+        //'CustomerAccount' => '33747501',
+        'CustomerAccount' => $account,
+        'Client' => 'Freshwork',
+        'OtherInfo' => '',
+        'RequestDate' => $date,
+        'Token' => $token,
+    );
+
+    $options = array(
+        'http' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'POST',
+        'content' => http_build_query($jsonData),
+    );
+
+$context  = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+var_dump($result);
+}
+else
+{
+    header('HTTP/1.0 403 Forbidden');
+    echo "Access Denied";
+}
+
+?>

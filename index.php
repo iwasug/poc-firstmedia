42ba130c7639fb79393483cfea1d0115f5866686
Iwa Sugriwa <iwa@radyalabs.com>
2020-03-22 07:30:08 +0700

    test timezone

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
-phpinfo();
-echo "tes";
+echo 'Default Timezone: ' . date('d-m-Y H:i:s') . '</br>';
+date_default_timezone_set('Asia/Jakarta');
+echo 'Indonesian Timezone: ' . date('d-m-Y H:i:s');
 ?>

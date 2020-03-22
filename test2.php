e8d1733064bbaf7aa4d159eb068d129d4638660d
Iwa Sugriwa <iwa@radyalabs.com>
2020-03-22 09:20:38 +0700

    test

--- test2.php
+++ test2.php
@@ -23,12 +23,13 @@ if (!empty($_POST['account'])) {
         'http' => array(
         'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
         'method'  => 'POST',
-        'content' => http_build_query($jsonData),
-    );
+        'content' => http_build_query($data),
+    )
+);
 
 $context  = stream_context_create($options);
 $result = file_get_contents($url, false, $context);
-echo($result);
+echo $result;
 }
 else
 {

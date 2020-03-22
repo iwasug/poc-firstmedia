0dbe74236acbb2a5629d86626c52a983fdb28f94
Iwa Sugriwa <iwa@radyalabs.com>
2020-03-22 07:54:50 +0700

    Sample POST to Fistmedia

--- index.php
+++ index.php
@@ -1,10 +1,34 @@
 <?php
 
 date_default_timezone_set('Asia/Jakarta');
-
-$formula = date('Y/m/d H:i:s') . '#FM#Freshwork#2020@fc9631fFreshwork';
+$date = date('Y-m-d H:i:s');
+$formula = $date . '#FM#Freshwork#2020@fc9631fFreshwork';
 $token = md5($formula);
 
-echo $formula . '-</br>' . $token; 
+//echo $formula . '</br>' . $token;
+
+$url = "https://fm1.firstmedia.com/FMCOMAPIRest/Content/Homepage";
+
+$jsonData = array(
+    'CustomerAccount' => '33747501',
+    'Client' => 'Client',
+    'OtherInfo' => '',
+    'RequestDate' => $date,
+    'Token' => $token,
+);
+
+$jsonDataEncoded = json_encode($jsonData);
+
+//Tell cURL that we want to send a POST request.
+curl_setopt($ch, CURLOPT_POST, 1);
+ 
+//Attach our encoded JSON string to the POST fields.
+curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
+ 
+//Set the content type to application/json
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json')); 
+ 
+//Execute the request
+$result = curl_exec($ch);
 
 ?>

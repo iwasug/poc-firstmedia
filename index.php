c2453a5519ff957306622ec1717c810be170ee5f
Iwa Sugriwa <iwa@radyalabs.com>
2020-03-22 08:23:46 +0700

    post

--- index.php
+++ index.php
@@ -6,34 +6,44 @@ $formula = $date . '#FM#Freshwork#2020@fc9631fFreshwork';
 $token = md5($formula);
 
 //echo $formula . '</br>' . $token;
+if (!empty($_GET['CustomerAccount'])) {
+    $url = "https://fm1.firstmedia.com/FMCOMAPIRest/Content/Homepage";
 
-$url = "https://fm1.firstmedia.com/FMCOMAPIRest/Content/Homepage";
+    $account = $_GET['CustomerAccount'];
 
-$jsonData = array(
-    'CustomerAccount' => '33747501',
-    'Client' => 'Freshwork',
-    'OtherInfo' => '',
-    'RequestDate' => $date,
-    'Token' => $token,
-);
+    $jsonData = array(
+        //'CustomerAccount' => '33747501',
+        'CustomerAccount' => $account,
+        'Client' => 'Freshwork',
+        'OtherInfo' => '',
+        'RequestDate' => $date,
+        'Token' => $token,
+    );
 
-$ch = curl_init($url);
+    $ch = curl_init($url);
 
-$jsonDataEncoded = json_encode($jsonData);
+    $jsonDataEncoded = json_encode($jsonData);
 
-//Tell cURL that we want to send a POST request.
-curl_setopt($ch, CURLOPT_POST, 1);
- 
-//Attach our encoded JSON string to the POST fields.
-curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
- 
-//Set the content type to application/json
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json')); 
- 
-//Execute the request
-$result = curl_exec($ch);
+    //Tell cURL that we want to send a POST request.
+    curl_setopt($ch, CURLOPT_POST, 1);
+    
+    //Attach our encoded JSON string to the POST fields.
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
+    
+    //Set the content type to application/json
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json')); 
+    
+    //Execute the request
+    $result = curl_exec($ch);
 
-echo json_encode($result);
-echo $jsonDataEncoded;
+
+    header("Content-Type: application/json; charset=UTF-8");
+    echo json_encode($result);
+}
+else
+{
+    header('HTTP/1.0 403 Forbidden');
+    echo "Access Denied";
+}
 
 ?>

7fbef5f3e1decc748012090f20bc292600d84c7e
Iwa Sugriwa <iwa@radyalabs.com>
2020-03-22 09:07:09 +0700

    test

--- test.php
+++ test.php
@@ -1,11 +1,7 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-
-$data = json_decode(file_get_contents("php://input"));
-
-echo $data;
+date_default_timezone_set('Asia/Jakarta');
+$date = date('Y-m-d H:i:s');
+$formula = $date . '#FM#Freshwork#2020@fc9631fFreshwork';
+$token = md5($formula);
+echo $formula . '</br>' . $token;
 ?>

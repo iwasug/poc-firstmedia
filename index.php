1ea4e5898120e20a6f5829dfba5c7e68fa8d79a0
Iwa Sugriwa <iwa@radyalabs.com>
2020-03-22 07:34:57 +0700

    test Formula

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
-echo 'Default Timezone: ' . date('d-m-Y H:i:s') . '</br>';
+
 date_default_timezone_set('Asia/Jakarta');
-echo 'Indonesian Timezone: ' . date('d-m-Y H:i:s');
+
+$formula = date('Y/m/d H:i:s') . '#FM#Freshwork#2020@fc9631fFreshwork';
+$token = md5($formula);
+
+echo $formula . '-</br>' . $token; 
+
 ?>

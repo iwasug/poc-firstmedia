852fe1605cd63e4a7a0b51ee0935ba47235b8092
Iwa Sugriwa <iwa@radyalabs.com>
2020-03-31 21:12:56 +0700

    Add Handover

--- Http_Functions.php
+++ Http_Functions.php
@@ -60,5 +60,44 @@
             $result = file_get_contents($url, false, $context);
             return $result;
          }
+
+
+         function handOver($conversationId)
+         {
+            $token = "eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJpS216TTVkenRIWmprdmdSY3VrVHgxTzJ2SFlTM0U5YmVJME9XbXRNR1ZzIn0.eyJqdGkiOiJmNDRhOGNhNS1jZjE4LTRjZDQtYTY2Yy0wOGNhNmZiZjk3ZjkiLCJleHAiOjE5MDEwMDY2OTcsIm5iZiI6MCwiaWF0IjoxNTg1NjQ2Njk3LCJpc3MiOiJodHRwOi8vaW50ZXJuYWwtZmMtdXNlMS0wMC1rZXljbG9hay1vYXV0aC0xMzA3MzU3NDU5LnVzLWVhc3QtMS5lbGIuYW1hem9uYXdzLmNvbS9hdXRoL3JlYWxtcy9wcm9kdWN0aW9uIiwiYXVkIjoiMTc5NGZjMmUtOWI0ZC00NGZhLTlkMDEtYWUyMDExODY1MDk0Iiwic3ViIjoiMjYwMWQ2MDQtZjE2Ny00MjhjLThiZGUtZDJmMzcyYmMzNDE5IiwidHlwIjoiQmVhcmVyIiwiYXpwIjoiMTc5NGZjMmUtOWI0ZC00NGZhLTlkMDEtYWUyMDExODY1MDk0IiwiYXV0aF90aW1lIjowLCJzZXNzaW9uX3N0YXRlIjoiYTg5N2JmNmMtY2FhMC00YTUxLWJjNDEtZmRjNDM2MjY3NGIzIiwiYWNyIjoiMSIsImFsbG93ZWQtb3JpZ2lucyI6W10sInJlYWxtX2FjY2VzcyI6eyJyb2xlcyI6WyJvZmZsaW5lX2FjY2VzcyIsInVtYV9hdXRob3JpemF0aW9uIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsiYWNjb3VudCI6eyJyb2xlcyI6WyJtYW5hZ2UtYWNjb3VudCIsIm1hbmFnZS1hY2NvdW50LWxpbmtzIiwidmlldy1wcm9maWxlIl19fSwic2NvcGUiOiJhZ2VudDp1cGRhdGUgbWVzc2FnZTpjcmVhdGUgYWdlbnQ6Y3JlYXRlIGRhc2hib2FyZDpyZWFkIHJlcG9ydHM6ZXh0cmFjdDpyZWFkIHJlcG9ydHM6cmVhZCBhZ2VudDpyZWFkIGNvbnZlcnNhdGlvbjp1cGRhdGUgdXNlcjpkZWxldGUgY29udmVyc2F0aW9uOmNyZWF0ZSBvdXRib3VuZG1lc3NhZ2U6Z2V0IG91dGJvdW5kbWVzc2FnZTpzZW5kIHVzZXI6Y3JlYXRlIHJlcG9ydHM6ZmV0Y2ggdXNlcjp1cGRhdGUgdXNlcjpyZWFkIHJlcG9ydHM6ZXh0cmFjdCBjb252ZXJzYXRpb246cmVhZCIsImNsaWVudEhvc3QiOiIxOTIuMTY4LjEyOC4xNTkiLCJjbGllbnRJZCI6IjE3OTRmYzJlLTliNGQtNDRmYS05ZDAxLWFlMjAxMTg2NTA5NCIsImNsaWVudEFkZHJlc3MiOiIxOTIuMTY4LjEyOC4xNTkifQ.LciZnXS7tcW8VnA3RBkOJpEBfAnj2RvGN2hlHqk3oPqoxecnpAvVqWvPS-tO9imc2-Q6lvUHh6Nz0SkpHhgYkpPpQTkJCxUOHQJ7lM4ODNE37ZuHWR8AUn3-qYhzkPOvx5o1zxDROlqAsmE0z0DjZcR8CrcrQSTjTUs6MIxOpLfwE6bacAk9ILEW18AzmVkFoo9Zz0x5uLq1gPSANcl88bMUTUuaB1fvmLsmAsSOkGvkpDgzSqcv0bxjJiItjPmH9Snqdxu7Yadt8pgnAaWQkJUbQy7Jb9KxJ5gAmml4sDmKyVbWXke45yKbfSTkob9USyYV9w6rwh2zpaZyEvTeFQ";
+            $agent = "c6759a15-d602-4a2a-8a71-72aa29a1cc4d";
+
+            $url = "https://api.freshchat.com/v2/conversations/$conversationId";
+
+            $jsonData = array(
+               //'CustomerAccount' => '33747501',
+               'conversation_id' => $conversationId,
+               'status' => 'assigned',
+               'assigned_agent_id' => $agent,
+               'app_id' => 'e1d62e57-2274-4054-94ec-df5f59bc98ce',
+
+           );
+
+            $data = json_encode($jsonData);
+
+            $headers = array(
+               'Content-Type: application/json',
+               'Authorization: Bearer '. $token
+           );
+
+            $opts = array('http' =>
+            array(
+                'method'  => 'PUT',
+                'header'  => $headers,
+                'content' => $data,
+                'timeout' => 60
+            )
+            );
+                                
+            $context  = stream_context_create($opts);
+            $result = file_get_contents($url, false, $context);
+            return $result;
+
+         }
     }
 ?>
--- index.php
+++ index.php
@@ -1,125 +1,171 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-    header('Method Not Allowed', true, 405);
-    echo "GET method requests are not accepted for this resource";
-    exit;
-}
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-
-date_default_timezone_set('Asia/Jakarta');
-$date = date('Y-m-d H:i:s');
-$formula = $date . '#FM#Freshwork#2020@fc9631fFreshwork';
-$token = md5($formula);
-$data = json_decode(file_get_contents("php://input"));
-if (!empty($data))
-{
-    $response = array("status" => 0, "message" => "", "data" => null);
-    $tag = $data->tag;
-    require_once 'DB_Functions.php';
-    require_once 'Http_Functions.php';
-    switch ($tag)
-	{
-        case 'account':
-            $url = "https://fm1.firstmedia.com/FMCOMAPIRest/Content/Homepage";
-            $account = $data->account;
-            $jsonData = array(
-                //'CustomerAccount' => '33747501',
-                'CustomerAccount' => $account,
-                'Client' => 'Freshwork',
-                'OtherInfo' => '',
-                'RequestDate' => $date,
-                'Token' => $token,
-            );
-
-            $http = new Http_Functions();
-        
-            $jsonDataEncoded = json_encode($jsonData);
-
-            $make_call = $http->callAPI_v2('POST', $url, $jsonDataEncoded);
-
-            $result = json_decode($make_call);
-
-            if($result->Rcode == "104")
-            {
-                $response["status"] = 200;
-                $response["message"] = "";
-                $response["data"] = $result;
-                echo json_encode($response);
-            }
-            else
-            {
-                $response["status"] = 500;
-                $response["message"] = $result->Result;
-                echo json_encode($response);
-            }
-
-        break;
-        case 'inbound':
-
-            $db = new DB_Functions();
-
-            $userId = $data->userId; 
-            $conversation = $data->conversation; 
-            $inbondCode = $data->inbondCode;
-
-            $conve = json_encode($conversation);
-
-            $ret = $db->insertInboundv2($userId, $conve, $inbondCode);
-
-            if($ret)
-            {
-                $response["status"] = 200;
-                $response["message"] = "Ok";
-                echo json_encode($response);
-            }
-            else
-            {
-                $response["status"] = 500;
-                $response["message"] = "InternalServerError";
-                echo json_encode($response);
-            }
-        break;
-        case 'get_inbound':
-
-            $db = new DB_Functions();
-
-            $userId = $data->userId; 
-            $inbondCode = $data->inbondCode;
-
-            $ret = $db->getInbound($userId, $inbondCode);
-
-            if(count($ret) != 0)
-            {
-                $response["status"] = 200;
-                $response["message"] = "";
-                $response["data"] = $ret;
-                echo json_encode($response);
-            }
-            else
-            {
-                $response["status"] = 500;
-                $response["message"] = "InternalServerError";
-                echo json_encode($response);
-            }
-        break;
-
-        default:
-            $response["status"] = 400;
-            $response["message"] = "Bad Request";
-            echo json_encode($response);
-        break;
+    // header('Method Not Allowed', true, 405);
+    // echo "GET method requests are not accepted for this resource";
+    // exit;
+    ?>
+     <!DOCTYPE html>
+    <html>
+    <head>
+    <title>POC - Fistmedia</title>
+    <script>
+    function initFreshChat() {
+        window.fcWidget.init({
+        token: "917622ba-1489-4c42-8811-554312319494",
+        host: "https://wchat.freshchat.com"
+        });
     }
-
-    header("Content-Type: application/json; charset=UTF-8");
+    function initialize(i,t){var e;i.getElementById(t)?initFreshChat():((e=i.createElement("script")).id=t,e.async=!0,e.src="https://wchat.freshchat.com/js/widget.js",e.onload=initFreshChat,i.head.appendChild(e))}function initiateCall(){initialize(document,"freshchat-js-sdk")}window.addEventListener?window.addEventListener("load",initiateCall,!1):window.attachEvent("load",initiateCall,!1);
+    </script>
+    </head>
+    <body>
+    <h1>POC - Fistmedia</h1>
+    </body>
+    </html> 
+    <?php
 }
 else
 {
-    header('Access Denied', true, 403);
-    echo "Access Denied";
-}
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+    header("Access-Control-Allow-Methods: POST");
+    header("Access-Control-Max-Age: 3600");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
+    date_default_timezone_set('Asia/Jakarta');
+    $date = date('Y-m-d H:i:s');
+    $formula = $date . '#FM#Freshwork#2020@fc9631fFreshwork';
+    $token = md5($formula);
+    $data = json_decode(file_get_contents("php://input"));
+    if (!empty($data))
+    {
+        $response = array("status" => 0, "message" => "", "data" => null);
+        $tag = $data->tag;
+        require_once 'DB_Functions.php';
+        require_once 'Http_Functions.php';
+        switch ($tag)
+        {
+            case 'account':
+                $url = "https://fm1.firstmedia.com/FMCOMAPIRest/Content/Homepage";
+                $account = $data->account;
+                $jsonData = array(
+                    //'CustomerAccount' => '33747501',
+                    'CustomerAccount' => $account,
+                    'Client' => 'Freshwork',
+                    'OtherInfo' => '',
+                    'RequestDate' => $date,
+                    'Token' => $token,
+                );
+
+                $http = new Http_Functions();
+            
+                $jsonDataEncoded = json_encode($jsonData);
+
+                $make_call = $http->callAPI_v2('POST', $url, $jsonDataEncoded);
+
+                $result = json_decode($make_call);
+
+                if($result->Rcode == "104")
+                {
+                    $response["status"] = 200;
+                    $response["message"] = "";
+                    $response["data"] = $result;
+                    echo json_encode($response);
+                }
+                else
+                {
+                    $response["status"] = 500;
+                    $response["message"] = $result->Result;
+                    echo json_encode($response);
+                }
+
+            break;
+            case 'handover':
+                $conversationId = $data->conversationId;
+
+                $http = new Http_Functions();
+            
+                $make_call = $http->handOver($conversationId);
+
+                $result = json_decode($make_call);
+
+                if($result != null)
+                {
+                    $response["status"] = 200;
+                    $response["message"] = "success";
+                    $response["data"] = $result;
+                    echo json_encode($response);
+                }
+                else
+                {
+                    $response["status"] = 500;
+                    $response["message"] = "Conversation not found";
+                    echo json_encode($response);
+                }
+
+            break;
+            case 'inbound':
+
+                $db = new DB_Functions();
+
+                $userId = $data->userId; 
+                $conversation = $data->conversation; 
+                $inbondCode = $data->inbondCode;
+
+                $conve = json_encode($conversation);
+
+                $ret = $db->insertInboundv2($userId, $conve, $inbondCode);
+
+                if($ret)
+                {
+                    $response["status"] = 200;
+                    $response["message"] = "Ok";
+                    echo json_encode($response);
+                }
+                else
+                {
+                    $response["status"] = 500;
+                    $response["message"] = "InternalServerError";
+                    echo json_encode($response);
+                }
+            break;
+            case 'get_inbound':
+
+                $db = new DB_Functions();
+
+                $userId = $data->userId; 
+                $inbondCode = $data->inbondCode;
+
+                $ret = $db->getInbound($userId, $inbondCode);
+
+                if(count($ret) != 0)
+                {
+                    $response["status"] = 200;
+                    $response["message"] = "";
+                    $response["data"] = $ret;
+                    echo json_encode($response);
+                }
+                else
+                {
+                    $response["status"] = 500;
+                    $response["message"] = "InternalServerError";
+                    echo json_encode($response);
+                }
+            break;
+
+            default:
+                $response["status"] = 400;
+                $response["message"] = "Bad Request";
+                echo json_encode($response);
+            break;
+        }
+
+        header("Content-Type: application/json; charset=UTF-8");
+    }
+    else
+    {
+        header('Access Denied', true, 403);
+        echo "Access Denied";
+    }
+}
 ?>
